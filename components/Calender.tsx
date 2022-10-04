e6045777ca870f481cc3be270531a89f1339f4b0
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-04 23:52:13 +0530

    SWEEP

--- components/Calender.tsx
+++ components/Calender.tsx
@@ -1,23 +1,3 @@
-import { CalendarList } from 'react-native-calendars'
-
-/**
- * @see https://github.com/wix/react-native-calendars
- */
 export const Calender = () => {
-  return (
-    <CalendarList
-      // Callback which gets executed when visible months change in scroll view. Default = undefined
-      onVisibleMonthsChange={(months) => {
-        console.log('now these months are visible', months)
-      }}
-      // Max amount of months allowed to scroll to the past. Default = 50
-      pastScrollRange={50}
-      // Max amount of months allowed to scroll to the future. Default = 50
-      futureScrollRange={50}
-      // Enable or disable scrolling of calendar list
-      scrollEnabled={true}
-      // Enable or disable vertical scroll indicator. Default = false
-      showScrollIndicator={true}
-    />
-  )
+  return <>Calender</>
 }
