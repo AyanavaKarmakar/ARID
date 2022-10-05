70972b764b301835c7cbdf1fa679c4b269f2c497
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-05 21:21:57 +0530

    Fixes Update Data List in Dropdown #27

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -4,42 +4,46 @@ import { Dropdown } from 'react-native-element-dropdown'
 import AntDesign from 'react-native-vector-icons/AntDesign'
 
 const data = [
-  { label: 'Andhra Pradesh', value: 'Andhra Pradesh' },
-  { label: 'Arunachal Pradesh', value: 'Arunachal Pradesh' },
-  { label: 'Assam', value: 'Assam' },
-  { label: 'Bihar', value: 'Bihar' },
-  { label: 'Chhattisgarh', value: 'Chhattisgarh' },
-  { label: 'Goa', value: 'Goa' },
-  { label: 'Gujarat', value: 'Gujarat' },
-  { label: 'Haryana', value: 'Haryana' },
-  { label: 'Himachal Pradesh', value: 'Himachal Pradesh' },
-  { label: 'Jharkhand', value: 'Jharkhand' },
-  { label: 'Karnataka', value: 'Karnataka' },
-  { label: 'Kerala', value: 'Kerala' },
-  { label: 'Madhya Pradesh', value: 'Madhya Pradesh' },
-  { label: 'Maharashtra', value: 'Maharashtra' },
-  { label: 'Manipur', value: 'Manipur' },
-  { label: 'Meghalaya', value: 'Meghalaya' },
-  { label: 'Mizoram', value: 'Mizoram' },
-  { label: 'Nagaland', value: 'Nagaland' },
-  { label: 'Odisha', value: 'Odisha' },
-  { label: 'Punjab', value: 'Punjab' },
-  { label: 'Rajasthan', value: 'Rajasthan' },
-  { label: 'Sikkim', value: 'Sikkim' },
-  { label: 'Tamil Nadu', value: 'Tamil Nadu' },
-  { label: 'Telangana', value: 'Telangana' },
-  { label: 'Tripura', value: 'Tripura' },
-  { label: 'Uttar Pradesh', value: 'Uttar Pradesh' },
-  { label: 'Uttarakhand', value: 'Uttarakhand' },
-  { label: 'West Bengal', value: 'West Bengal' },
-  { label: 'Andaman and Nicobar Islands', value: 'Andaman and Nicobar Islands' },
-  { label: 'Chandigarh', value: 'Chandigarh' },
-  { label: 'Dadra Nagar Haveli and Daman Diu', value: 'Dadra Nagar Haveli and Daman Diu' },
-  { label: 'Delhi', value: 'Delhi' },
-  { label: 'Jammu and Kashmir', value: 'Jammu and Kashmir' },
-  { label: 'Ladakh', value: 'Ladakh' },
-  { label: 'Lakshadweep', value: 'Lakshadweep' },
-  { label: 'Puducherry', value: 'Puducherry' },
+  {
+    label: 'Andaman and Nicobar Islands',
+    value: 'Andaman and Nicobar Islands',
+  },
+  {
+    label: 'Delhi',
+    value: 'Delhi',
+  },
+  {
+    label: 'Jammu',
+    value: 'Jammu',
+  },
+  {
+    label: 'Kashmir',
+    value: 'Kashmir',
+  },
+  {
+    label: 'Karnataka',
+    value: 'Karnataka',
+  },
+  {
+    label: 'Kerala',
+    value: 'Kerala',
+  },
+  {
+    label: 'Maharashtra',
+    value: 'Maharashtra',
+  },
+  {
+    label: 'Rajasthan',
+    value: 'Rajasthan',
+  },
+  {
+    label: 'Tamil Nadu',
+    value: 'Tamil Nadu',
+  },
+  {
+    label: 'West Bengal',
+    value: 'West Bengal',
+  },
 ]
 
 /**
