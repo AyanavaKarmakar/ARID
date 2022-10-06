5ea6a95aa9570d23e8b312c7dd9ea2ed9e4639c3
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-06 22:16:50 +0530

    feat: Add Slice Reducers to the Store

--- store.ts
+++ store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
+import regionReducer from './features/region/regionSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    region: regionReducer,
+  },
 })
 
 export type RootState = ReturnType<typeof store.getState>
