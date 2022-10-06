3aab761f5dfd387bee3c76114fc8d83a7031f190
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-07 00:59:03 +0530

    feat: provide context

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { StatusBar } from 'expo-status-bar'
 import { Home } from './screens'
+import { RegionContext } from './RegionContext'
+import { useState } from 'react'
 
 export type NativeRootStackParamList = {
   Home: undefined
@@ -15,12 +17,22 @@ const globalScreenOptions = {
 }
 
 export default function App() {
+  const [region, setRegion] = useState('West Bengal')
+
+  const toggleRegion = (newRegion: string) => {
+    setRegion(newRegion)
+  }
+
+  const contextValue = { region: region, setRegion: toggleRegion }
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={globalScreenOptions}>
-        <Stack.Screen name='Home' component={Home} />
-      </Stack.Navigator>
-      <StatusBar style='light' />
-    </NavigationContainer>
+    <RegionContext.Provider value={contextValue}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={globalScreenOptions}>
+          <Stack.Screen name='Home' component={Home} />
+        </Stack.Navigator>
+        <StatusBar style='light' />
+      </NavigationContainer>
+    </RegionContext.Provider>
   )
 }
