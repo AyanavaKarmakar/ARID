def98fa8096990da72223fe15eeb6b64cf837a18
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-06 23:28:16 +0530

    CLEAN SWEEP

--- App.tsx
+++ App.tsx
@@ -2,8 +2,6 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { StatusBar } from 'expo-status-bar'
 import { Home } from './screens'
-import { store } from './store'
-import { Provider } from 'react-redux'
 
 export type NativeRootStackParamList = {
   Home: undefined
@@ -18,13 +16,11 @@ const globalScreenOptions = {
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={globalScreenOptions}>
-          <Stack.Screen name='Home' component={Home} />
-        </Stack.Navigator>
-        <StatusBar style='light' />
-      </NavigationContainer>
-    </Provider>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen name='Home' component={Home} />
+      </Stack.Navigator>
+      <StatusBar style='light' />
+    </NavigationContainer>
   )
 }
