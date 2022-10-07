ef29aa5b927d9d8c25e6aed9b932fb75c2f5fd01
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-07 11:36:29 +0530

    feat: setup loading animation

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,3 +1,15 @@
+import { MD3Colors, ProgressBar } from 'react-native-paper'
+import { useEffect, useState } from 'react'
+
 export const Loading = () => {
-  return <>Loading</>
+  const [progressValue, setProgressValue] = useState(0)
+  const DELAY_MS = 100
+
+  useEffect(() => {
+    setInterval(() => {
+      if (progressValue < 1.0) setProgressValue((previousState) => previousState + 0.1)
+    }, DELAY_MS)
+  }, [])
+
+  return <ProgressBar progress={progressValue} color={MD3Colors.error50} />
 }
