86d243fc7f400377d197ebf74f1dd913c67f5b2a
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-07 11:02:33 +0530

    feat: card style for web

--- components/Info.tsx
+++ components/Info.tsx
@@ -1,17 +1,53 @@
-import { Button, Card, Title, Paragraph } from 'react-native-paper'
+import { Card, Paragraph, Title } from 'react-native-paper'
+import { StyleSheet } from 'react-native'
 
 export const Info = () => {
   return (
-    <Card>
-      <Card.Content>
-        <Title>Card title</Title>
-        <Paragraph>Card content</Paragraph>
+    <Card mode='elevated' elevation={3} style={styles.cardContainer}>
+      <Card.Title
+        title='NOT a dry day!'
+        subtitle='Next dry day is in X days'
+        titleNumberOfLines={1}
+        titleVariant={'headlineSmall'}
+        titleStyle={styles.cardTitleStyle}
+        subtitleNumberOfLines={1}
+        subtitleVariant={'bodyLarge'}
+        subtitleStyle={styles.cardSubtitleStyle}
+      />
+      <Card.Content style={styles.cardContentStyle}>
+        <Title style={styles.cardContentTitleStyle}>Region Selected: Region</Title>
+        <Paragraph style={styles.cardContentParagraphStyle}>Extra Info</Paragraph>
       </Card.Content>
-      <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
-      <Card.Actions>
-        <Button>Cancel</Button>
-        <Button>Ok</Button>
-      </Card.Actions>
     </Card>
   )
 }
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingStart: 10,
+    paddingEnd: 10,
+    backgroundColor: '#1F2022',
+  },
+  cardTitleStyle: {
+    textAlign: 'center',
+    color: '#87CEEB',
+  },
+  cardSubtitleStyle: {
+    textAlign: 'center',
+    color: '#E0FFFF',
+  },
+  cardContentStyle: {
+    paddingTop: 15,
+    paddingBottom: 10,
+  },
+  cardContentTitleStyle: {
+    textAlign: 'center',
+    color: '#87CEEB',
+  },
+  cardContentParagraphStyle: {
+    textAlign: 'center',
+    color: '#E0FFFF',
+  },
+})
