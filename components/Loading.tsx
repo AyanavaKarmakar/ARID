2772129d6867424e746e7c985ca86cc5043d5cdc
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-10-07 11:39:03 +0530

    fix: variable name

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -3,12 +3,12 @@ import { useEffect, useState } from 'react'
 
 export const Loading = () => {
   const [progressValue, setProgressValue] = useState(0)
-  const DELAY_MS = 100
+  const INTERVAL_MS = 100
 
   useEffect(() => {
     setInterval(() => {
       if (progressValue < 1.0) setProgressValue((previousState) => previousState + 0.1)
-    }, DELAY_MS)
+    }, INTERVAL_MS)
   }, [])
 
   return <ProgressBar progress={progressValue} color={MD3Colors.error50} />
